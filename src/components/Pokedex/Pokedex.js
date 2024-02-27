79876ecfecd8ecd31a64718dc66cf38fa5c84f77
Mark Rattle <mark.rattle@gmail.com>
2024-02-27 11:16:44 -0500

    pagination

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -8,15 +8,42 @@ import './Pokedex.css';
 
 function Pokedex() {
   const pokemon = useSelector(state => state.pokedex.pokemon);
-  let page = useSelector(state => state.pokedex.currentPage);
+  let currentPage = useSelector(state => state.pokedex.currentPage);
+
+  // currentPage = 2
 
   console.log("Pokedex component")
   console.log(pokemon)
+
+  // pagination
+  const pokemonPerPage = 6;
+
+  function pokemonRange(page, pokemonPerPage) {
+    
+    const maxIndex = Object.keys(pokemon).length
+    let startIndex = (page - 1) * pokemonPerPage <= (maxIndex - pokemonPerPage)
+      ? (page - 1) * pokemonPerPage + 1
+      : maxIndex - pokemonPerPage + 1
+
+    let indices = []
+
+    for (let i = startIndex; i < startIndex + pokemonPerPage; i++) {
+      if (i > maxIndex) break;
+      indices.push(i);
+    }
+
+    return indices
+  }
+
+  let displayedPokemon = pokemonRange(currentPage, pokemonPerPage)
+  console.log("displayedPokemon")
+  console.log(displayedPokemon)
+
   return (
     <div className="Pokedex">
       <h1>This is the Pokedex</h1>
       <div className="pokemonList">
-        {Object.keys(pokemon).map((key) => {
+        {displayedPokemon.map((key) => {
           console.log("key: " + key)
           return (
           <PokemonListCard 
